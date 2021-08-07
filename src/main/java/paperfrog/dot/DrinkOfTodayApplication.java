f23c23961b438cd9a52ecca5cfb3de06d441ac36
paperfrog <paperfrog@naver.com>
2021-08-07 22:50:16 +0900

    init project
    Spring boot : 2.5.3
    Java : 11(jar)
    Build tool : Gradle
    Dependencies : SpringWeb, Lombok, thymeleaf

---
+++ src/main/java/paperfrog/dot/DrinkOfTodayApplication.java
@@ -0,0 +1,13 @@
+package paperfrog.dot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkOfTodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkOfTodayApplication.class, args);
+	}
+
+}

3da04475291452096f7013a9e392fde3deea9a58
paperfrog <paperfrog@naver.com>
2021-08-09 22:03:33 +0900

    Add save and find feature
    단순한 저장 기능과 검색기능 그리고 테스트 코드.
    map <board id, board> 로 save 기능을 구현하였고
    board의 id로 검색하는 findById,
    boardRepository에 저장되있는 모든 board를 리스트 형식으로 반환하는 findAll을 구현하였다

--- src/main/java/paperfrog/dot/boardservice/domain/board/BoardRepository.java
+++ src/main/java/paperfrog/dot/boardservice/domain/board/BoardRepository.java
@@ -1,6 +1,8 @@
 package paperfrog.dot.boardservice.domain.board;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,6 +10,20 @@ public class BoardRepository {
     private static final Map<Long,Board> store=new ConcurrentHashMap<>();
     private Long sequence=0L;
     private Date date;
+    public Board save(Board board){
+        board.setId(++sequence);
+        date=new Date();
+        board.setDate(date);
+        store.put(board.getId(), board);
+        return board;
+    }
+    public Board findById(Long id){
+        return store.get(id);
+    }
+    public List<Board> findAll(){
+        List<Board> list=new ArrayList<Board>(store.values());
+        return list;
+    }
     public void clear(){
         store.clear();
     }

c6f80c57288dc4c9fd1f3ab39ada3cc181fcb2ee
paperfrog <paperfrog@naver.com>
2021-08-10 22:51:21 +0900

    Add click button to board
    타임 리프를 통해 board list에서 게시물 선택 시 해당 게시물로 이동하는 기능을 구현했다.(board.id 를 통해 찾음)

--- src/main/java/paperfrog/dot/boardservice/web/BoardController.java
+++ src/main/java/paperfrog/dot/boardservice/web/BoardController.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import paperfrog.dot.boardservice.domain.board.Board;
 import paperfrog.dot.boardservice.domain.board.BoardRepository;
@@ -24,7 +25,7 @@ public class BoardController {
     public void addTestBoard(){
         for(int i=1;i<=100;i++) {
             Date date = new Date();
-            boardRepository.save(new Board(date, "title1"+i, "content"+i));
+            boardRepository.save(new Board(date, "title"+i, "content"+i));
         }
     }
     @RequestMapping("/list")
@@ -32,5 +33,11 @@ public class BoardController {
         List<Board> boardList=boardRepository.findAll();
         model.addAttribute("boardList",boardList);
         return "board/boardlist";
+    }
+    @GetMapping("/{boardId}")
+    public String item(Model model, @PathVariable long boardId){
+        Board board = boardRepository.findById(boardId);
+        model.addAttribute("board",board);
+        return "board/view/board";
     }
 }

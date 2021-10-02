37b16a2da0fd50746fedc5e36c3707335afc82d3
paperfrog <paperfrog@naver.com>
2021-10-02 19:58:57 +0900

    Add key stretching feature at encrypt
    암호화에 키스트레칭을 추가하여 보안을 더 강화하였다.
    키 스트레칭 반복 횟수는 마찬가지로 서버 내 프로퍼티에 개인저장 해놓을 생각이다.

--- src/main/java/paperfrog/dot/web/EncryptManager.java
+++ src/main/java/paperfrog/dot/web/EncryptManager.java
@@ -1,19 +1,18 @@
 package paperfrog.dot.web;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 @Component
 public class EncryptManager {
-    @Value("${salt}")
-    String salt="dd";
+    @Value("${encrypt.salt}")
+    String salt="";
+    @Value("${encrypt.iteration}")
+    int iteration=;
     public String encrypt(String text) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] plain=addSalt(text,salt);
-        md.update(plain);
-        return bytesToHex(md.digest());
+        return bytesToHex(iteratorDigest(plain));
     }
     private String bytesToHex(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
@@ -27,5 +26,15 @@ public class EncryptManager {
         System.arraycopy(text.getBytes(),0,saltPlain,0,text.getBytes().length);
         System.arraycopy(salt.getBytes(),0,saltPlain,text.getBytes().length,salt.getBytes().length);
         return saltPlain;
+    }
+    private byte[] iteratorDigest(byte[] plain) throws NoSuchAlgorithmException {
+        System.out.println("iter  : "+iteration);
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] inputDigest = md.digest(plain);
+        for(int i=0;i<iteration;i++){
+            md.reset();
+            inputDigest = md.digest(inputDigest);
+        }
+        return inputDigest;
     }
 }

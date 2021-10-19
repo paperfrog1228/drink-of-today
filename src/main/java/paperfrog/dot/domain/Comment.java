045f4a0c0de90f0162131715a50e9531fd7a7a21
paperfrog <paperfrog@naver.com>
2021-10-19 19:38:09 +0900

    Add comment domain class
    댓글 도메인 클래스 생성하였으며 게시글과의 관계를 단방향으로 구현하였는데 이게 양방향이 필요할 지 잘 모르겠다. 다른 레이어 기능 구현하면서 바꿔보던가 해야겠다

---
+++ src/main/java/paperfrog/dot/domain/Comment.java
@@ -0,0 +1,26 @@
+package paperfrog.dot.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+    댓글 기능
+ **/
+@Entity
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String writer;
+    private String date;
+    private String text;
+    private String content;
+    @ManyToOne(cascade = CascadeType.ALL) //단방향 해도 괜찮지 않을까?
+    private Board board;
+}
